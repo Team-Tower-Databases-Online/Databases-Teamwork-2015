4af2904245658beb032912a45b0b12407592b4a6
SDaskalov <daskalovstanimir@gmail.com>
2015-10-19 11:27:26 +0300

    Added excel reports functionality

--- CloudTemple/CloudTemple.ConsoleClient/Program.cs
+++ CloudTemple/CloudTemple.ConsoleClient/Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace CloudTemple.ConsoleClient
 {
     using Logic;
-
+    using Excel;
     internal class Program
     {
         private static readonly DataSeeder DataSeeder = new DataSeeder();
@@ -9,13 +9,15 @@
         private static readonly PdfReportGenerator PdfReportGenerator = new PdfReportGenerator();
         private static readonly XmlReportsHandler XmlReportsHandler = new XmlReportsHandler();
         private static readonly JsonReportsGenerator JsonReportsGenerator = new JsonReportsGenerator();
+        private static readonly ExcelPrinter ExcelFileGenerator = new ExcelPrinter();
 
         private static void Main()
         {
             // TODO: Improve logic by implementing factory design pattern
             // Preparation
+             
             DataSeeder.Seed();
-
+          
             // Problem 1
             ExcelReportsLoader.Load();
 
@@ -30,6 +32,10 @@
 
             // Problem 5
             XmlReportsHandler.Save();
+
+            //Problem 6
+            ExcelFileGenerator.GenrateExcel();
+
         }
     }
 }
---
+++ CloudTemple/CloudTemple.Logic/ExcelPrinter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Runtime.InteropServices;
+using SQL = System.Data;
+using Microsoft.Office.Interop.Excel;
+
+namespace CloudTemple.Excel
+{
+    public class ExcelPrinter
+    {
+
+        public ExcelPrinter()
+        {
+
+        }
+        public void GenrateExcel()
+        {
+            SQLiteConnection con;
+            con = new SQLiteConnection("Data Source = C:\\SqLiteDb.sqlite; Version=3;");
+            con.Open();
+            string sql = "SELECT * FROM ProductTaxes";
+            SQLiteCommand command = new SQLiteCommand(sql, con);
+            SQLiteDataReader reader = command.ExecuteReader();
+            
+            Application application;
+            Workbook workBook;
+            Worksheet workSheet;
+
+            application = new Application();
+            application.Visible = true;
+
+            workBook = (Workbook)(application.Workbooks.Add(Missing.Value));
+            workSheet = (Worksheet)workBook.ActiveSheet;
+
+            try
+            {
+                workSheet.Name = "dataBase";
+                workSheet.Cells[1, "A"] = "ProductName";
+                workSheet.Cells[1, "B"] = "Amount";
+
+                    int row = 2;
+                while (reader.Read())
+                {
+                   
+
+                    workSheet.Cells[row, "A"] = reader["ProductName"];
+                    workSheet.Cells[row, "B"] = reader["Amount"];
+
+                    row += 1;
+                    application.Visible = false;
+                    application.UserControl = false;
+
+                    
+                }
+                workBook.SaveAs(@"C:\Users\Miro\Documents\GitHub\Databases-Teamwork-2015\Reports\Excel_Reports\ExcelReportNumer");
+                workSheet.Columns[1].AutoFit();
+                workSheet.Columns[2].AutoFit();
+                ((Range)workSheet.Columns[1]).AutoFit();
+                ((Range)workSheet.Columns[2]).AutoFit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Marshal.ReleaseComObject(workBook);
+            }
+
+            con.Close();
+        }
+    }
+}
+
